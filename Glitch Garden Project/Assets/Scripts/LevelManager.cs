225b6c0ec39b8bc29ae2ede5f51a72550ed51178
david <davss0802@gmail.com>
2020-06-12 17:05:32 +0400

    Music Manager

--- Glitch Garden Project/Assets/Scripts/LevelManager.cs
+++ Glitch Garden Project/Assets/Scripts/LevelManager.cs
@@ -2,28 +2,33 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
 
- public float autoLoadNextLevelAfter;
-   
-
-    void Start() {
-	 Invoke("LoadNextLevel",autoLoadNextLevelAfter);
- }
+    public float autoLoadNextLevelAfter;
 
 
+    void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
 
-	public void LoadLevel(string name){
-		Debug.Log ("New Level load: " + name);
-		SceneManager.LoadScene (name);
-	}
 
-	public void QuitRequest(){
-		Debug.Log ("Quit requested");
-		Application.Quit();
-	}
-	
-	public void LoadNextLevel() {
-		Application.LoadLevel(Application.loadedLevel + 1);
-	}
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
 }
---
+++ Glitch Garden Project/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] levelMusicChangeArray;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+        if (thisLevelMusic)
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+
+
+}
